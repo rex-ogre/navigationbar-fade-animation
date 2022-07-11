092646d075fb848fbfc8e23a13bf6751d1c54374
rex <rex>
2022-07-11 22:11:33 +0800

    Initial Commit

---
+++ goonsUITests/goonsUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  goonsUITestsLaunchTests.swift
+//  goonsUITests
+//
+//  Created by 陳冠雄 on 2022/7/11.
+//
+
+import XCTest
+
+class goonsUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
